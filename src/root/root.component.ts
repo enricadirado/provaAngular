f628332f9bf4b4ae7cddf276194e1cb9ad236035
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-06-06 00:27:13 +0200

    02

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit, VERSION } from '@angular/core';
+import { TempCity } from './temp-city';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone:true,
   templateUrl: './root.component.html',
-  styleUrls: ['./root.component.css']
+  styleUrls: ['./root.component.css'],
+  imports: [CommonModule]
 })
 export class RootComponent implements OnInit {
 
   title: string = 'Temperature in Angular ' + VERSION.major;
-  
+  cities: Array<TempCity> = [
+    new TempCity('Torino', '14'),
+    new TempCity('Milano', '15'),
+    new TempCity('Genova', '18'),
+  ];
   constructor() { }
 
   ngOnInit() {
