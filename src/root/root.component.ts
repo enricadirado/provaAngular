59364b0bd29c1ecd3b245f944f41b814aa5b791d
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-06-06 12:21:12 +0200

    2 files modified

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -10,15 +10,24 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule]
 })
 export class RootComponent implements OnInit {
-
   title: string = 'Temperature in Angular ' + VERSION.major;
   cities: Array<TempCity> = [
     new TempCity('Torino', '14'),
     new TempCity('Milano', '15'),
     new TempCity('Genova', '18'),
   ];
-  constructor() { }
+  seleziona(name: string) {
+    var trovato: Array<TempCity> = this.cities.filter(
+      el => ( el.nome === name )
+    );
+    this.selezione = trovato[0];
+  }
+  selezione: TempCity|undefined;
+  clean() {
+    this.selezione = undefined;
+  }
 
+  constructor() { }
   ngOnInit() {
   }
 
